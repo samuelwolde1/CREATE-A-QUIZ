93587d039dffac44feeca1f6936964af934fb0a1
samuelwolde1 <126281252+samuelwolde1@users.noreply.github.com>
2023-05-26 11:21:32 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+# Create a Quiz By Samuel Wolde
+
+print("WELCOME TO THE HISTORY QUIZ")
+
+# Input 
+quiz_score = 0
+
+#Question 1
+print("Whose death lead to the start of WW1?")
+answer1 = input("Q1 Answer: ").lower()
+
+if answer1 == "archduke franz ferdinand" or answer1 == "franz ferdinand" or answer1 == "archduke ferdinand":
+    print("CORRECT")
+    quiz_score+= 1
+else:
+    print("INCORRECT")
+
+#Question 2
+print("What side was the United States on in WW1?")
+answer2 = input("Q2 Answer: ").lower()
+
+if answer2 == "allies":
+    print("CORRECT")
+    quiz_score+= 1
+else:
+    print("INCORRECT")
+
+#Question 3
+print("Whose death lead to the start of WW1?")
+answer1 = input("Q1 Answer: ").lower()
+
+if answer1 == "archduke franz ferdinand" or answer1 == "franz ferdinand" or answer1 == "archduke ferdinand":
+    print("CORRECT")
+    quiz_score+= 1
+else:
+    print("INCORRECT")
